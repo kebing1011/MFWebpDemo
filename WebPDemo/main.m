38b5d8587d1fe50e0b2368c4750bb98c63888699
maokebing <maokebing@aspirecn.com>
2015-12-18 10:13:44 +0800

    init

---
+++ WebPDemo/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  WebPDemo
+//
+//  Created by Mao on 12/7/15.
+//  Copyright © 2015 00000. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WebP/encode.h>
+#import <WebP/decode.h>
+#import <AppKit/AppKit.h>
+
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+		
+		NSImage* image = [[NSImage alloc] initWithContentsOfFile:@"/Users/Mao/Pictures/1.jpg"];
+		
+		
+		
+	}
+    return 0;
+}
